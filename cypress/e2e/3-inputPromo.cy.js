718549acf4f085446f1315dbfa10fdf903b995ab
jahfal <jahfal.lazuardi@sinarmasland.com>
2024-09-09 16:07:56 +0700

    feat: add test case 4 (choose expedition)

--- cypress/e2e/3-inputPromo.cy.js
+++ cypress/e2e/3-inputPromo.cy.js
@@ -13,30 +13,6 @@ describe("Test case 3", () => {
     cy.get('.chakra-input').type('QRP-TEST-123');
     cy.get('.style_input-promo-container__xV2HC > .chakra-button').click();
     cy.xpath("//*[name()='path' and contains(@d,'M16.192 6.')]").click();
-    //   cy.get('#chakra-modal--header-1 > svg').eq(0).click();
-    //   cy.xpath('//label[2]//span[1]').click();
-    //   cy.xpath("//button[normalize-space()='Lanjut']").click();
-    //   cy.xpath("//input[@placeholder='Isi dengan nama lengkap Anda']").type("Reza");
-    //   cy.get('[style="margin-right: 1rem; margin-bottom: 0px;"] > .chakra-input__group > .chakra-input').type("082462453");
-    //   cy.get('[style="margin: 16px 0px;"] > .chakra-input__group > .chakra-input').type("Sml@12345");
-    //   cy.wait(1000)
-    //   cy.xpath("//button[normalize-space()='Daftar']").click();
-    //   cy.get('#chakra-modal--body-5 > .chakra-button').click();
-    //   cy.get('#pin-input-13-0').type("1")
-    //   cy.get('#pin-input-13-1').type("2")
-    //   cy.get('#pin-input-13-2').type("3")
-    //   cy.get('#pin-input-13-3').type("4")
-    //   cy.get('#pin-input-13-4').type("5")
-    //   cy.get('#pin-input-13-5').type("6")
-    //   cy.get('.css-15icixv > .chakra-button').click()
-    //   cy.get('.chakra-link').click()
-  
-      
-      
-      // cy.xpath('body > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)').type("test@example.com");
-      // cy.wait(1000);
-      // cy.get("a[href='/VehicleChecklistAdmin/ManageLokasi?LocationId=0']").click();
-      // cy.wait(1000)
-      // cy.xpath("(//span)[14]").click();
+    
     });
   });
---
+++ cypress/e2e/4-chooseExpedition.cy.js
@@ -0,0 +1,18 @@
+// require('cypress-xpath');
+import Login from "./login";
+
+describe("Test case 4", () => {
+    beforeEach(() => {
+      const loginPage = new Login();
+      loginPage.loginAccount();
+    });
+    it("Choose Expedition", function () {
+    cy.xpath("//*[name()='path' and contains(@d,'M21.822 7.')]").click();
+    cy.get("button[class='chakra-button css-1pilj7v']").click();
+    cy.get("path[d='M1.9225 9.75581L6.67834 4.99997L1.9225 0.244141L0.744171 1.42247L4.32167 4.99997L0.744171 8.57747L1.9225 9.75581Z']").click();
+    cy.xpath('//div[2]//label[1]//div[1]//input[1]').click();
+    cy.xpath("//p[normalize-space()='jnt']").click();
+    // cy.xpath("//*[name()='path' and contains(@d,'M16.192 6.')]").click();
+    
+    });
+  });
