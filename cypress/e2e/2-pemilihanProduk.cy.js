aabefbbb1a6a571bdb59d84ae946eb684164afa2
jahfal <jahfal.lazuardi@sinarmasland.com>
2024-09-09 15:30:34 +0700

    fix: adjustment toggle button

--- cypress/e2e/2-pemilihanProduk.cy.js
+++ cypress/e2e/2-pemilihanProduk.cy.js
@@ -7,19 +7,27 @@ describe("Test case 2", () => {
   });
 
   it("Activated toggle button", function () {
-
-    cy.xpath("//li[1]//div[1]//*[name()='svg']").eq(0).click();
-
-    cy.get('.chakra-input').type('Collagen drink {enter}');
-    cy.wait(1000);
-
-    cy.get('.styles_card-info__uYTN4').eq(0).click();
-
-    cy.wait(5000); 
-    cy.xpath("//button[@id='productDetail-button-counter']//*[name()='svg']")
-      .should('be.visible') 
-      .click({ force: true }); 
-
-
+    activateToggleButton();
   });
 });
+
+function activateToggleButton() {
+
+  cy.xpath("//li[1]//div[1]//*[name()='svg']").eq(0).click();
+
+
+  cy.get('.chakra-input').type('Collagen drink {enter}');
+  cy.wait(1000);
+
+
+  cy.get('.styles_card-info__uYTN4').eq(0).click();
+
+
+  cy.wait(5000); 
+  cy.xpath("//button[@id='productDetail-button-counter']//*[name()='svg'][2]/*[name()='path'][1] ")
+    .should('be.visible')
+    .click({ multiple: true, force: true }) 
+    .click({ multiple: true, force: true })
+    .click({ multiple: true, force: true })
+    .click({ multiple: true, force: true })
+}
