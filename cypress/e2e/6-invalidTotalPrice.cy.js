d0694e364f7eb29cbea0fb03f42e655f5d7f9fac
jahfal <jahfal.lazuardi@sinarmasland.com>
2024-09-09 16:23:47 +0700

    feat: add test case 6 (invalid total price)

---
+++ cypress/e2e/6-invalidTotalPrice.cy.js
@@ -0,0 +1,38 @@
+import Login from "./login";
+
+describe("Test case 6", () => {
+  beforeEach(() => {
+    const loginPage = new Login();
+    loginPage.loginAccount();
+  });
+
+  it("Activated toggle button & Add to basket", function () {
+    activateToggleButton();
+  });
+});
+
+function activateToggleButton() {
+
+  cy.xpath("//li[1]//div[1]//*[name()='svg']").eq(0).click();
+
+
+  cy.get('.chakra-input').type('Collagen drink {enter}');
+  cy.wait(1000);
+
+
+  cy.get('.styles_card-info__uYTN4').eq(0).click();
+
+
+  cy.wait(5000); 
+  cy.xpath("//button[@id='productDetail-button-counter']//*[name()='svg'][2]/*[name()='path'][1] ")
+    .should('be.visible')
+    .click({ multiple: true, force: true }) 
+    .click({ multiple: true, force: true })
+    .click({ multiple: true, force: true })
+
+  cy.xpath("//h1[normalize-space()='Beli sekarang']").click({ multiple: true, force: true })
+  cy.get('#chakra-modal--header-23 > .chakra-text').should(
+    "have.text",
+    "Belum menjadi Member"
+  )
+}
