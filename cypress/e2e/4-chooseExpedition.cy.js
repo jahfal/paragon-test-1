6fc1c4c198c087df095b600cd8087a1b177219e8
jahfal <jahfal.lazuardi@sinarmasland.com>
2024-09-09 16:08:58 +0700

    fix: clean unused code on test case 4

--- cypress/e2e/4-chooseExpedition.cy.js
+++ cypress/e2e/4-chooseExpedition.cy.js
@@ -12,7 +12,6 @@ describe("Test case 4", () => {
     cy.get("path[d='M1.9225 9.75581L6.67834 4.99997L1.9225 0.244141L0.744171 1.42247L4.32167 4.99997L0.744171 8.57747L1.9225 9.75581Z']").click();
     cy.xpath('//div[2]//label[1]//div[1]//input[1]').click();
     cy.xpath("//p[normalize-space()='jnt']").click();
-    // cy.xpath("//*[name()='path' and contains(@d,'M16.192 6.')]").click();
     
     });
   });
